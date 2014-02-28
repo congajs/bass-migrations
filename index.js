d3de53f1019802bbd40891f91a07e7a50bab9c95
Anthony Matarazzo <email@anthonymatarazzo.com>
2014-02-28 04:11:47 +0000

    initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+/*
+ * This file is part of the bass-migrations module.
+ *
+ * (c) Anthony Matarazzo <email@anthonymatarazzo.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+/**
+ * Export the public constructors
+ *
+ * @author Anthony Matarazzo <email@anthonymatarazzo.com>
+ */
+module.exports = {
+	BaseCommand: require('./lib/BaseCommand') ,
+	Migration: require('./lib/Migration') ,
+	Helper: require('./lib/Helper')
+};
